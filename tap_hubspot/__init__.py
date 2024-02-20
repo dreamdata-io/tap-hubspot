af7dc73abf55b5c2717bd0440fdf315ff41cc38d
YannisMag <mageirasjohn@gmail.com>
2024-02-20 11:00:36 +0100

    use updatedAt for line_items

--- tap_hubspot/__init__.py
+++ tap_hubspot/__init__.py
@@ -41,7 +41,7 @@ ADVANCED_STREAMS = {
     "tasks": {"bookmark_key": "lastUpdated"},
     "emails": {"bookmark_key": "lastUpdated"},
     "campaigns": {},
-    "line_items": {}
+    "line_items": {"bookmark_key": "updatedAt"}
 }
 
 REQUIRED_CONFIG_KEYS = [
